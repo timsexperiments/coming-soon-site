e891a35e3bf27142658438900cfd259c6a7579a5
Tim <tim@timsexperiments.foo>
2023-09-28 14:08:30 -0500

    Change success state to no longer accept input

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,7 @@
 export const SUBSCRIBED_USERS_KEY = 'subscribed';
+export const PREVIOUS_ATTEMPT_COOKIE_KEY = 'previous-attempt';
+export const SUBSCRIPTION_EMAIL_TEMPLATE = 'SUBSCRIPTION';
+
+export type AlertType = 'error' | 'success';
+export const SUCCESS: AlertType = 'success';
+export const ERROR: AlertType = 'error';
