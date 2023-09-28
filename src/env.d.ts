901dd55faf3a3643767d71f33a3f11e4bee6ad44
Tim <tim@timsexperiments.foo>
2023-09-28 12:57:09 -0500

    Finish coming soon site with animation

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,12 +2,17 @@
 
 type AdvancedRuntime = import('@astrojs/cloudflare').AdvancedRuntime;
 type KVNamespace = import('@cloudflare/workers-types').KVNamespace;
-type SendEmail = import('@cloudflare/workers-types').SendEmail;
+type ServiceWorkerGlobalScope =
+  import('@cloudflare/workers-types').ServiceWorkerGlobalScope;
 
 declare namespace App {
   interface Locals extends AdvancedRuntime {
     runtime: AdvancedRuntime & {
-      env: { NEWS_LETTER_KV: KVNamespace; NO_REPLY_EMAIL: SendEmail };
+      env: {
+        NEWS_LETTER_KV: KVNamespace;
+        EMAILER_SERVICE: ServiceWorkerGlobalScope;
+        API_KEY: string;
+      };
     };
   }
 }
---
+++ src/scripts/index.ts
@@ -0,0 +1,5 @@
+import { initCollapses } from 'flowbite';
+
+document.addEventListener('DOMContentLoaded', function (_) {
+  initCollapses();
+});
